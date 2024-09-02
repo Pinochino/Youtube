62a5b4a5b4db460a4caced86bdcf6104e7ad946d
Pinochino <tranhunghp22112004@gmail.com>
2024-09-02 21:50:35 +0700

    resonsive product page

--- JS/main.js
+++ JS/main.js
@@ -26,26 +26,20 @@ function toggleButtonLogo() {
 window.addEventListener('load', buttonCategories);
 
 function buttonCategories() {
-    const categoriesButton = qsa('.category-button');
+    const categoriesButton = qsa('.categories .category-button');
     const categoriesContent = qsa('.container');
 
     const categoriesButtonArray = Array.from(categoriesButton);
     const categoriesContentArray = Array.from(categoriesContent);
 
-
-    // for (let index = 0; index < categoriesContentArray.length; index++) {
-    //    console.log(categoriesContentArray[index]);
-        
-    // }
-    
     // Tabs button
     categoriesButtonArray.forEach((tab, index) => {
         tab.addEventListener('click', () => {
-            categoriesButtonArray.forEach((tab) => tab.classList.remove('active') )
+            categoriesButtonArray.forEach((tab) => tab.classList.remove('active'))
             tab.classList.add('active')
-            
-                    categoriesContentArray.forEach(data => data.classList.remove('active'))
-                    categoriesContentArray[index].classList.add('active')
+
+            categoriesContentArray.forEach(data => data.classList.remove('active'))
+            categoriesContentArray[index].classList.add('active')
         })
     })
 
@@ -61,4 +55,62 @@ function buttonCategories() {
         lastItem.innerHTML = `<i class='bx bx-chevron-right' style="font-size: 25px; width: 25px; height: 25px; color: black"></i>`;
     }
 }
+
+window.addEventListener('load', Carousel);
+
+function Carousel() {
+    const carousel = document.querySelector('.carousel');
+    const firstImg = document.querySelectorAll('.card')[0];
+    const arrowIcons = document.querySelectorAll('.carousel i');
+    
+    let isDragStart = false, prevPageX, prevScrollLeft;
+    
+    const showHideIcon = () => {
+        let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // getting max scrollable width
+        arrowIcons[0].style.display = carousel.scrollLeft === 0 ? 'none' : 'block';
+        arrowIcons[1].style.display = carousel.scrollLeft === scrollWidth ? 'none' : 'block';
+    };
+    
+    arrowIcons.forEach(icon => {
+        icon.addEventListener('click', () => {
+            let firstImgWidth = firstImg.clientWidth + 14; // getting first image width & adding 14 margin value
+            // if clicked icon is left, reduce with value from the carousel scroll left else add to it
+            carousel.scrollLeft += icon.id === 'left' ? -firstImgWidth : firstImgWidth;
+            setTimeout(() => showHideIcon(), 60);
+        });
+    });
+
+    const dragStart = (e) => {
+        // updating global variables value on mouse down event
+        isDragStart = true;
+        prevPageX = e.pageX || e.touches[0].pageX;
+        prevScrollLeft = carousel.scrollLeft;
+    };
+
+    const dragging = (e) => {
+        // scrolling images/carousel to left according to mouse pointer
+        if (!isDragStart) return;
+        e.preventDefault();
+        carousel.classList.add('dragging');
+        let positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+        carousel.scrollLeft = prevScrollLeft - positionDiff;
+        showHideIcon();
+    };
+
+    const dragStop = () => {
+        isDragStart = false;
+        carousel.classList.remove('dragging');
+    };
+
+    carousel.addEventListener('mousedown', dragStart);
+    // carousel.addEventListener('touchstart', dragStart);
+
+    carousel.addEventListener('mousemove', dragging);
+    // carousel.addEventListener('touchmove', dragging);
+
+    carousel.addEventListener('mouseup', dragStop);
+    carousel.addEventListener('mouseleave', dragStop);
+    carousel.addEventListener('touchend', dragStop);
+}
+
 
