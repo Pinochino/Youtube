b955d3da01c25eee0e45f6705fe3e3d95d6af32d
Pinochino <tranhunghp22112004@gmail.com>
2024-09-17 20:45:56 +0700

    Adjust Responsive Product page (1)

--- JS/main.js
+++ JS/main.js
@@ -9,18 +9,7 @@ function qsa(selector) {
     return document.querySelectorAll(selector);
 }
 
-window.addEventListener('load', toggleButtonLogo);
-window.addEventListener('resize', toggleButtonLogo);
 
-
-function toggleButtonLogo() {
-    const logo = qs('.user');
-    if (window.innerWidth <= 600) {
-        logo.innerHTML = '<i class="fa-solid fa-magnifying-glass"></i>';
-    } else {
-        logo.innerHTML = 'LOGO';
-    }
-}
 
 // Category button
 window.addEventListener('DOMContentLoaded', buttonCategories);
